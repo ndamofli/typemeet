b8b58d3dbea36090b85d0d8a25c91c3ff02b55b5
Nickolas Damofli <nickolas@damofli.com>
2025-10-10 18:36:03 +0300

    Inngest API error fixed (serve methods)

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,9 +2,8 @@ import { inngest } from "@/inngest/client";
 import { serve } from "inngest/next";
 import { syncCreatedUser, syncUpdatedUser } from "@/inngest/functions"; // Your own functions
 
-// All serve handlers have the same arguments:
-serve({
-  client: inngest, // a client created with new Inngest()
+export const { GET, POST, PUT } = serve({
+  client: inngest,
   functions: [syncCreatedUser, syncUpdatedUser], // an array of Inngest functions to serve, created with inngest.createFunction()
   /* Optional extra configuration */
 });
